1522126c424d2c212515efa5caa6583db27761bf
mirwoz <miranda.wozmak@gmail.com>
2019-06-03 04:34:25 -0400

    Admin page is functional

--- client/src/components/WeekSelector.js
+++ client/src/components/WeekSelector.js
@@ -1,31 +1,29 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { FormGroup, Label, Input } from 'reactstrap';
 
-
 class WeekSelector extends React.Component {
-    render() {
-        const {
-            two_weeks_ago,
-            last_week,
-            this_week,
-            next_week,
-            following_week,
-            handleTimeSheetChange
-             } = this.props;
-        return (
-    <FormGroup>
-     <Label for="exampleSelect">Select A Timesheet</Label>
+  render() {
+    const {
+      two_weeks_ago,
+      last_week,
+      this_week,
+      next_week,
+      following_week,
+      handleTimeSheetChange,
+    } = this.props;
+    return (
+      <FormGroup>
+        <Label for="exampleSelect">Select A Timesheet</Label>
         <Input type="select" onChange={handleTimeSheetChange} name="select">
-          <option value={this_week}>{this_week}</option>
           <option value={two_weeks_ago}>{two_weeks_ago}</option>
           <option value={last_week}>{last_week}</option>
-          <option value={this_week}>{this_week}</option>
+          <option value={this_week}>{this_week} This Week</option>
           <option value={next_week}>{next_week}</option>
           <option value={following_week}>{following_week}</option>
         </Input>
-    </FormGroup>
-        );
-    }
-};
+      </FormGroup>
+    );
+  }
+}
 
-export default WeekSelector
+export default WeekSelector;
